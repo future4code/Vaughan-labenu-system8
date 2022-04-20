e347631fa281c85d46f75016b9942042fc1d085e
Gledson Lucas <gledsonlucas111@gmail.com>
2022-04-20 17:03:14 -0300

    terceiro commit

--- src/classes/Estudantes.ts
+++ src/classes/Estudantes.ts
@@ -1,13 +1,17 @@
 import { Usuarios } from "./Usuarios";
 
 export class Estudantes extends Usuarios{
+    hobbies: string[]
+
 constructor(
     nome: string,
     email: string,
     data_nasc: string,
-    turma_id: string
+    turma_id: string,
+    hobbies: string[]
     ){
     super(nome, email, data_nasc, turma_id)
+    this.hobbies = hobbies
 }
 
 }
