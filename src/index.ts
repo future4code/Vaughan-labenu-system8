153ae4554164f4bdce078ebe636824cbf82fbdd1
Gledson Lucas <gledsonlucas111@gmail.com>
2022-04-22 12:50:24 -0300

    docentes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 import { app } from "./app";
+import { criarDocente } from "./endpoints/criarDocente";
+import { mudarDocenteTurma } from "./endpoints/mudarDocenteTurma";
+import { pegarDocentes } from "./endpoints/pegarDocentes";
+
+
+app.post("/docentes", criarDocente);
+
+app.get("/docentes", pegarDocentes);
+
+app.put("/docentes", mudarDocenteTurma);
+
+
+
+
+
 
