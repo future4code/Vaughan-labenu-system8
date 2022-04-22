f6b719725bd3be548dfcd673838e004cd2aa49f1
GeysonMes <geysonsousa17@gmail.com>
2022-04-22 15:19:10 -0300

    Estudantes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import { app } from "./app";
+import { buscarEstudante } from "./endpoints/Estudantes/buscarEstudante";
+import { criarEstudante } from "./endpoints/Estudantes/criarEstudante";
+import { mudarEstudanteTurma } from "./endpoints/Estudantes/mudarEstudanteTurma";
 
+app.post ("/estudantes",criarEstudante)
+app.get ("/estudantes/:nome",buscarEstudante)
+app.put ("/estudantes/:idDoEstudante",mudarEstudanteTurma)
