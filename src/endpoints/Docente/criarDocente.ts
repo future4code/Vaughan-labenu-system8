5bf73f15449303dcb3ba2e6ce6b285ece4c5cd56
Gledson Lucas <gledsonlucas111@gmail.com>
2022-04-22 14:48:11 -0300

    alteracoes docente

---
+++ src/endpoints/Docente/criarDocente.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { Docentes } from "../classes/Docentes";
+import { connection } from "../data/connection";
+
+
+
+
+
+export const criarDocente = async(req: Request, res: Response): Promise<void> => {
+    let errorCode: number = 400;
+    try{
+        const {nome, email, dataNasc, turma_id } = req.body;
+
+        if(!nome || !email || !dataNasc || !turma_id) {
+            errorCode = 422
+            throw new Error("Todos os campos do body precisam ser preenchidos!");
+        }
+
+        const id = Date.now().toString();
+
+        const data_nasc = dataNasc.split('/').reverse().join('-');
+
+        const docente: Docentes = new Docentes(id, nome, email, dataNasc, turma_id);
+
+        await connection("DOCENTE").insert({id, nome, email, data_nasc, turma_id});
+        
+        res.status(201).send({message: "Docente criado!"});
+
+    }catch(error: any) {
+        res.status(errorCode).send({message: error.sqlMessage || error.message });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
-import { criarDocente } from "./endpoints/criarDocente";
-import { mudarDocenteTurma } from "./endpoints/mudarDocenteTurma";
-import { pegarDocentes } from "./endpoints/pegarDocentes";
+import { criarDocente } from "./endpoints/Docente/criarDocente";
+import { mudarDocenteTurma } from "./endpoints/Docente/mudarDocenteTurma";
+import { pegarDocentes } from "./endpoints/Docente/pegarDocentes";
 
 
 app.post("/docentes", criarDocente);
